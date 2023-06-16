f12bebc9ed6a5a68fbed81a2f4f83b8ed6701da0
Jeongseop <prayer525@mtn.co.kr>
2023-06-16 17:57:14 +0900

    #fix : init 페이지 적용

---
+++ src/page/VMDetail.js
@@ -0,0 +1,17 @@
+import "../assets/css/_detail.scss";
+
+const VMDetail = () => {
+  return (<>
+    <div style={{background: '#eff4f9', padding: '20px'}}>
+      <div className="d-flex">
+        <div className="box-radius">
+          <div className="vm-detail-header">
+            fdasfdafdafdsa
+          </div>
+        </div>
+      </div>
+    </div>
+  </>)
+}
+
+export default VMDetail
